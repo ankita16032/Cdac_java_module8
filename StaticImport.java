82831e4e72a597bbe53aff939104097e21dab008
ankita16032 <ankitagadgemetiit29@gmail.com>
2024-10-03 22:33:51 +0530

    Add files via upload

---
+++ StaticImport.java
@@ -0,0 +1,20 @@
+package module8;
+
+import static java.lang.Math.*;
+
+public class StaticImport {
+
+	public static void main(String[] args) {
+		System.out.println(pow(3, 4));
+		System.out.println(pow(2, 3));
+		System.out.println(pow(9, 2));
+
+		System.out.println(random());
+		System.out.println(random());
+		System.out.println(round(33.4));
+		System.out.println(round(19.6));
+		System.out.println(sqrt(25));
+
+	}
+
+}
